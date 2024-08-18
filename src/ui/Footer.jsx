9efb41ec5fde298a6a2ed59b97ee2534a5f66c5b
lamnt2501 <trung.lam.for.work@gmail.com>
2024-08-19 00:52:57 +0700

    feat: add footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,5 +1,22 @@
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="flex justify-between bg-white p-4 text-slate-400">
+      <p className="">
+        <span className="uppercase">copyright &copy;</span> 2024 QMShop All
+        rights Reserved
+      </p>
+      <p>
+        Made by{" "}
+        <a
+          href="https://github.com/lamnt2501/qmshop-admin"
+          target="_blank"
+          className="text-main"
+        >
+          Lamdangfixbug
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
