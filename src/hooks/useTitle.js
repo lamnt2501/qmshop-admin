896df85f103fe7c70d9e329450fc7e9731192e7b
lamnt2501 <trung.lam.for.work@gmail.com>
2024-09-03 21:34:38 +0700

    fix: add clear function

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -3,5 +3,7 @@ import { useLayoutEffect } from "react";
 export default function useTitle(title) {
   useLayoutEffect(() => {
     document.title = title;
+
+    return () => (document.title = "QM Shop");
   }, [title]);
 }
