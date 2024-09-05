b4065d69c07768e2f74b9f9bb53de666bb2a1f8b
lamnt2501 <trung.lam.for.work@gmail.com>
2024-09-05 23:02:32 +0700

    style: error page

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,12 +1,37 @@
 import { useNavigate, useRouteError } from "react-router-dom";
 
+const errorMessage = (error) => {
+  switch (error.status) {
+    case 404:
+      return "Page Not Found";
+    default:
+      return "Something Went Wrong!";
+  }
+};
+
 function Error() {
   const navigate = useNavigate();
   const error = useRouteError();
+  console.log(error);
   return (
-    <div>
-      <h1>{error.data || error.message}</h1>
-      <button onClick={() => navigate(-1)}>Trở lại</button>
+    <div className="flex h-[100dvh] items-center justify-center space-x-4 p-14">
+      <div className="flex-1">
+        <img
+          src="https://img.freepik.com/premium-vector/website-maintenance-repair-concept_108061-1877.jpg?w=996"
+          alt="page fixing image"
+        />
+      </div>
+      <div className="flex flex-1 flex-col items-center justify-center space-y-4">
+        <h1 className="text-center text-[40px] font-extrabold text-red-600">
+          {errorMessage(error)}
+        </h1>
+        <button
+          className="max-w-[200px] rounded-md bg-main px-4 py-3 text-lg text-white"
+          onClick={() => navigate(-1)}
+        >
+          ⬅️ Go Back
+        </button>
+      </div>
     </div>
   );
 }
